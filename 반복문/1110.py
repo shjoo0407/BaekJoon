b2dcf873a6d75661a546cd7018734cf2ef666ec1
shjoo0407 <69780999+shjoo0407@users.noreply.github.com>
2023-02-20 21:24:53 +0900

    last commit

--- 반복문/1110.py
+++ 반복문/1110.py
@@ -1,14 +1,21 @@
 # 더하기 사이클
 first_num = input()
+cnt = 0
+if int(first_num)<10:
+    first_num = "0"+first_num
+else:
+    pass
 
-while True:
-    tmp = first_num
-    if int(tmp)<10:
-        tmp = "0" + tmp
-    else:
-        sum = str(int(tmp[0])+int(tmp[1]))
-        new = first_num[-1]+sum[-1]
+sum = str(int(first_num[0])+int(first_num[1]))
+new = first_num[-1] + sum[-1]
+cnt = cnt + 1
 
-    if 
+while new != first_num:
+    sum = str(int(new[0])+int(new[1]))
+    new = new[-1] + sum[-1]
+    cnt = cnt + 1
+
+print(cnt)
+
 
 
