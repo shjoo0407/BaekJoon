749bcdbdcf3b22396e0e5466afe7a8ddb114deff
shjoo0407 <69780999+shjoo0407@users.noreply.github.com>
2023-02-27 23:18:23 +0900

    1316

--- 심화1/1316.py
+++ 심화1/1316.py
@@ -3,6 +3,16 @@ num = int(input())
 cnt = num
 for i in range(num):
     word = input()
-    for j in range(len(word)):
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        else:
+            if word[j] in word[j+1:]:
+                cnt = cnt-1
+                break
+print(cnt)
+
+
+
         
 
