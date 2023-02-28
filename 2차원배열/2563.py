0b5b9d3bd3c7b09d189cbb379108f154917614b1
shjoo0407 <69780999+shjoo0407@users.noreply.github.com>
2023-02-28 21:19:08 +0900

    2563

--- 2차원배열/2563.py
+++ 2차원배열/2563.py
@@ -1,12 +1,9 @@
 # 색종이
 num = int(input())
-paper = [[0] * 50]* 50
-
-print(paper)
+paper = [[0] * 100 for i in range(100)]
 
 for i in range(num):
     x, y = map(int,input().split())
-    print(x,y)
     for row in range(x,x+10):
         for col in range(y,y+10):
             paper[row][col] = 1
@@ -15,11 +12,7 @@ result = 0
 for i in paper:
     result = result + i.count(1)
 print(result)
-# cnt = 0
-# for i in range(100):
-#     cnt = cnt + sum(paper[i])
-#
-# print(cnt)
+
 
 
 
