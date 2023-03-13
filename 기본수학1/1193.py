390486f0bb2156acd543ab6b5e70bc80de0c391e
shjoo0407 <69780999+shjoo0407@users.noreply.github.com>
2023-03-13 23:33:37 +0900

    1193

---
+++ 기본수학1/1193.py
@@ -0,0 +1,16 @@
+num = int(input())
+line=1
+
+while num>line:
+    num = num - line
+    line = line+1
+
+if line%2 == 0:
+    a = num
+    b = line-num+1
+else:
+    a = line-num+1
+    b = num
+
+print(a,b,sep='/')
+
