95e94e5fb0afb340293a9dc2afa962394e8f70dd
shjoo0407 <69780999+shjoo0407@users.noreply.github.com>
2023-03-16 19:14:40 +0900

    2775

---
+++ 기본수학1/2775.py
@@ -0,0 +1,11 @@
+# 부녀회장이 될테야
+t = int(input())
+
+for i in range(t):
+    k = int(input())
+    n = int(input())
+    #0층 리스트
+    f = [x for x in range(1,n+1)]
+    for floor in range(k):
+        for num in range(n):
+            f[]
