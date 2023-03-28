c05c3ab1009811c6cf93aec646fc31a2d5568697
shjoo0407 <69780999+shjoo0407@users.noreply.github.com>
2023-03-28 22:31:09 +0900

    Add files via upload

--- 브루트포스/19532.py
+++ 브루트포스/19532.py
@@ -1,13 +1,7 @@
 # 수학은 비대면 강의입니다.
 
 a,b,c,d,e,f = map(int,input().split())
-x=0
-y=0
-for i in range(-999,1000):
-    for j in range(-999,1000):
-        if ((a*i)+(b*j)==c)&((d*i)+(e*j)==f):
-            x,y = i,j
 
+x = (c*e-b*f)//(a*e-b*d)
+y = (c*d-a*f)//(b*d-a*e)
 print(x,y)
-
-
