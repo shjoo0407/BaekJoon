305753380443168177cafb084412bcf2766f875c
shjoo0407 <69780999+shjoo0407@users.noreply.github.com>
2023-03-28 22:21:30 +0900

    correct but time limit

--- 브루트포스/19532.py
+++ 브루트포스/19532.py
@@ -1,2 +1,13 @@
 # 수학은 비대면 강의입니다.
 
+a,b,c,d,e,f = map(int,input().split())
+x=0
+y=0
+for i in range(-999,1000):
+    for j in range(-999,1000):
+        if ((a*i)+(b*j)==c)&((d*i)+(e*j)==f):
+            x,y = i,j
+
+print(x,y)
+
+
