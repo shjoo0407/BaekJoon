e9fe31ae9ecac8d57bae80e0d999830448bcc21e
shjoo0407 <69780999+shjoo0407@users.noreply.github.com>
2023-03-31 02:30:18 +0900

    1436

---
+++ 브루트포스/1436.py
@@ -0,0 +1,13 @@
+# 영화감독 숌
+num = int(input())
+count = 0
+i = 0
+while True:
+    i = i+1
+    if '666' in str(i):
+        count = count + 1
+        if num == count:
+            print(i)
+            break
+
+
